ea3ca4e0d369836fa0c83032e8e288597384f134
Akin Olusanya <66926466+AkinCodes@users.noreply.github.com>
2022-05-06 19:13:06 +0100

    testing

--- app/src/main/java/com/example/akintestproject/MainActivity.java
+++ app/src/main/java/com/example/akintestproject/MainActivity.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-     hello
+     // hello
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
